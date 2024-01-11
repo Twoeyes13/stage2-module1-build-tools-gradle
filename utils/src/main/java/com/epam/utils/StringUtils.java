36987b9ab03c28d70196b33d889235db528ad610
Nursultan Dulatov <80988817+Twoeyes13@users.noreply.github.com>
2024-01-11 16:28:38 +0600

    Seventh test!

--- utils/src/main/java/com/epam/utils/StringUtils.java
+++ utils/src/main/java/com/epam/utils/StringUtils.java
@@ -3,6 +3,11 @@ package com.epam.utils;
 public class StringUtils {
     public static boolean isPositiveNumber(String str) {
          //here magic will happen
-        return Integer.parseInt(str)>-1;
+        if (str == null || str.isEmpty())  return false;
+        if (str.matches("-?\\d+(\\.\\d+)?")) {
+            if (Double.parseDouble(str) < 1) return false;
+        } else return false;
+        return true;
+        //return Integer.parseInt(str)>-1;
     }
 }
